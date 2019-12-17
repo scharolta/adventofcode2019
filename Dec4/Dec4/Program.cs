29da31e9b1e500eea8be4b0e50d583efecdb8344
Scharolta Siencnik <scharolta.siencnik@eurostep.com>
2019-12-17 11:20:43 +0100

    Refine part4

--- Dec4/Dec4/Program.cs
+++ Dec4/Dec4/Program.cs
@@ -1,37 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dec4
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] _)
         {
-            int validPasswords = 0;
+            int validPasswordsPart1 = 0;
+            int validPasswordsPart2 = 0;
+
             for (int i = 307237; i <= 769058; i++)
             {
-                if (ApplyRules(i))
-                {
-                    validPasswords++;
-                }
+                var digitString = i.ToString();
+                if (ApplyRules(digitString, c => c >= 2)) validPasswordsPart1++;
+                if (ApplyRules(digitString, c => c == 2)) validPasswordsPart2++;
             }
-            Console.WriteLine(validPasswords);
+
+            Console.WriteLine($"Answer1: {validPasswordsPart1}\nAnswer2: {validPasswordsPart2}");
         }
 
-        private static bool ApplyRules(int number)
+        //ORIGINAL SOLUTION
+        private static bool ApplyRulesPart1(string digits)
         {
             var minDigit = 0;
-            var numberString = number.ToString();
+            bool sameAdjacent = false;
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                int parsedDigit = int.Parse(digits[i].ToString());
+
+                if (parsedDigit > minDigit) minDigit = parsedDigit;
+                else if (parsedDigit == minDigit) sameAdjacent = true;
+                else return false;
+            }
+
+            return sameAdjacent;
+        }
+        private static bool ApplyRulesPart2(string digits)
+        {
+            var minDigit = 0;
             int sameAdjacent = 1;
             bool twoAdjacent = false;
 
-            for (int i = 0; i < numberString.Length; i++)
+            for (int i = 0; i < digits.Length; i++)
             {
-                int parsedDigit = (int)char.GetNumericValue(numberString[i]);
-                
+                int parsedDigit = (int)char.GetNumericValue(digits[i]);
+
                 if (parsedDigit > minDigit)
                 {
                     minDigit = parsedDigit;
@@ -43,11 +58,18 @@ namespace Dec4
                 {
                     if (!twoAdjacent) sameAdjacent++;
                 }
-                
+
                 else return false;
             }
 
             return sameAdjacent == 2 || twoAdjacent;
+        }
+
+        //SMARTER SOLUTION (very compressed and not so readable)
+        private static bool ApplyRules(string digits, Func<int, bool> countEval)
+        {
+            return Enumerable.SequenceEqual(digits, digits.OrderBy(d => d)) 
+                && digits.GroupBy(d => d).Any(g => countEval(g.Count()));
         }
     }
 }
